c9f9d72a74fae7444ab0984ad6d3f36506150965
Parvat R <vidyutparvat2@gmail.com>
2024-08-08 11:04:18 +0530

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -13,13 +13,22 @@ def index():
     ...
 
 
-
 @app.get("/login")
 def login_get():
     ...
 
 @app.post("/login")
 def login_post():
+    ...
+
+
+
+@app.get("/admin_login")
+def admin_login_get():
+    ...
+
+@app.post("/admin_login")
+def admin_login_post():
     ...
 
 
@@ -43,8 +52,49 @@ def s_functions():
     ...
 
 @app.get("/s/f/<int:function_id>")
-def s_function_view():
+def s_function_view(function_id: int):
     ...
+
+
+@app.get("/s/scanqr")
+def s_scanqr():
+    ...
+
+
+
+@app.get("/a/f")
+def a_functions():
+    ...
+
+
+@app.get("/a/f/create")
+def a_function_create_get():
+    ...
+
+@app.post("/a/f/create")
+def a_function_create_post():
+    ...
+
+
+@app.get("/a/f/<int:function_id>")
+def a_function_view_get(function_id: int):
+    ...
+
+
+@app.post("/a/f/<int:function_id>")
+def a_function_view_post(function_id: int):
+    ...
+
+
+@app.post("/a/f/<int:function>/delete")
+def a_function_view_delete_post(function_id: int):
+    ...
+
+
+@app.post("/a/f/<int:function_id>/<int:session_id>/delete")
+def a_function_session_delete(function_id: int, session_id: int):
+    ...
+
 
 
 if __name__ == "__main__":
